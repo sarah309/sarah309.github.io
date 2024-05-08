8063f80e275363c9cd3a925ac364e44c6429e65d
sarah309 <sarah309@users.noreply.github.com>
2024-05-08 13:03:28 -0600

    equations

---
+++ cse110/w03/equations.py
@@ -0,0 +1,4 @@
+area_rectangle = float(length_rectange) * float(width_rectangle)
+print(f"The area of the rectangle is: {area_rectangle} units^2")
+#print("The area of the rectangle is: " + str(area_rectangle) + " units^2")
+print()
