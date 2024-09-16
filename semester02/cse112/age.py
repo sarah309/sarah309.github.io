8d716129d3963fc6d3df07816832d61aa490a008
Sarah Egendoerfer <egen1310@gmail.com>
2024-09-16 09:57:31 -0600

    programming refreshers

---
+++ semester02/cse112/age.py
@@ -0,0 +1,21 @@
+x = 10 # Set x to 10
+
+my_comment = '''This is a docstring or a multiline comment
+that can run
+across multiple lines (not a true comment)'''
+
+print(my_comment)
+
+# Ctrl + / comments out code
+
+# variables have a name, type and value
+# decimal numbers (1/3, 234.234, 23/8, etc.) are floats, 10 is an integer
+# boolean is True or false
+# string example my_name = 'Bob'
+# list example my_data = [1, 2, 3, 4] my_data_names = ['Bob', 'Tara']
+
+x = 120
+print(f"{x} is the value of x")
+
+age = input('Please input your age: ')
+print(f"Your age is: {age}")
