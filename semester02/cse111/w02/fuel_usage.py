31493e8218390cd6d764b20568e3597548506d19
Sarah Egendoerfer <egen1310@gmail.com>
2024-09-28 22:44:50 -0600

    wk 2 completed

--- semester02/cse111/w02/fuel_usage.py
+++ semester02/cse111/w02/fuel_usage.py
@@ -1,17 +1,23 @@
 def main():
+
     # Get an odometer value in U.S. miles from the user.
     start_miles = float(input('\nEnter the first odometer value in U.S. miles: '))
+
     # Get another odometer value in U.S. miles from the user.
     end_miles = float(input('Enter the second odometer value in U.S. miles: '))
+
     # Get a fuel amount in U.S. gallons from the user.
     amount_gallons = float(input('Enter the amount of fuel used in U.S. gallons: '))
+
     # Call the miles_per_gallon function and store
     # the result in a variable named mpg.
     mpg = miles_per_gallon(start_miles, end_miles, amount_gallons)
+
     # Call the lp100k_from_mpg function to convert the
     # miles per gallon to liters per 100 kilometers and
     # store the result in a variable named lp100k.
     lp100k = lp100k_from_mpg(mpg)
+    
     # Display the results for the user to see.
     print(f'\n{mpg:.1f} miles per gallon')
     print(f'{lp100k:.2f} liters per 100 kilometers\n')
